a0a213955cc83903b0d7ecfa8230c39058e0d3d6
Seonu <seonup95@gmail.com>
2021-09-05 14:50:37 +0900

    correction

--- src/environment.js
+++ src/environment.js
@@ -1,12 +1,12 @@
 // 위치, 날씨
 //const linkAPI = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API%20key};
 
-function onGeoOk() {
+function onGeoOk(position) {
     console.log(position);
 }
 
 function onGeoError() {
-    alert("Can't find your location");
+    alert("Can't find you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
