340d4911998e4e4837aff97fb6c41de48987e889
Seonu <seonup95@gmail.com>
2021-09-05 15:25:09 +0900

    update

--- src/environment.js
+++ src/environment.js
@@ -8,8 +8,16 @@ function onGeoOk(position) {
 
     //openweathermap.org API
     const key = "1129fffe00ecb17bfb0cb3409162ddb2";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const geoloction = document.querySelector(".geoloction");
+            const weather = document.querySelector(".weather");
+            geoloction.innerText = data.name;
+            weather.innerText = data.weather[0].main;
+        }
+    );
 }
 
 function onGeoError() {
@@ -17,8 +25,3 @@ function onGeoError() {
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
-const geoloction = document.querySelector(".geoloction");
-const weather = document.querySelector(".weather");
-
- 
